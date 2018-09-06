20db0904584d2daf845e6449b8e5225460be1664
lee <1303908401@qq.com>
2018-09-06 20:23:15 +0800

    no message

--- UniversalApp/Modules/FirstViewController/Controller/LTFirstViewController.m
+++ UniversalApp/Modules/FirstViewController/Controller/LTFirstViewController.m
@@ -7,70 +7,182 @@
 //
 
 #import "LTFirstViewController.h"
+#import "LTPrivacyPermission.h"
 
-@interface LTFirstViewController () <UIScrollViewDelegate> 
+@interface LTFirstViewController () <UITableViewDelegate, UITableViewDataSource>
+
+@property (strong, nonatomic) UITableView *tableView;
+@property (strong, nonatomic) NSArray *titleArray;
 
 @end
 
 @implementation LTFirstViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
-    self.title = @"第一个";
+    self.view.backgroundColor = [UIColor whiteColor];
+    [self initUI];
+}
+#pragma mark - UI
+- (void)initUI
+{
+    _titleArray = @[NSLocalizedString(@"打开相册权限", nil),
+                    NSLocalizedString(@"打开相机权限", nil),
+                    NSLocalizedString(@"打开媒体资料库权限", nil),
+                    NSLocalizedString(@"打开麦克风权限", nil),
+                    NSLocalizedString(@"打开位置权限", nil),
+                    NSLocalizedString(@"打开推送权限", nil),
+                    NSLocalizedString(@"打开语音识别权限", nil),
+                    NSLocalizedString(@"打开日历权限", nil),
+                    NSLocalizedString(@"打开通讯录权限", nil),
+                    NSLocalizedString(@"打开提醒事项权限", nil),
+                    NSLocalizedString(@"打开运动与健身权限", nil)
+                    ];
     
-    for (int i = 0; i < 4; i++) {
-        UIImageView *image = [self imageViewWithColor:UIColorHex(0x1E90FF) size:CGSizeMake(kScreenWidth - 20.f, 44) direction:i];
-        image.centerX = self.view.centerX;
-        image.top = 86 + i * 44.f;
-        [self.view addSubview:image];
+    _tableView = [[UITableView alloc] init];
+    _tableView.frame = CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height);
+    _tableView.delegate = self;
+    _tableView.dataSource = self;
+    _tableView.showsVerticalScrollIndicator = NO;
+    _tableView.scrollEnabled = YES;
+    _tableView.tableFooterView = [UIView new];
+    [self.view addSubview:_tableView];
+}
+
+#pragma mark - <UITableViewDataSource,UITableViewDelegate>
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 11;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return 50;
+}
+
+- (void)tableView:(UITableView *)tableView willDisplayCell:(nonnull UITableViewCell *)cell forRowAtIndexPath:(nonnull NSIndexPath *)indexPath
+{
+    if ([cell respondsToSelector:@selector(setSeparatorInset:)])
+    {
+        [cell setSeparatorInset:UIEdgeInsetsMake(0, 0, 0, 0)];
     }
-    
-    
-    CGFloat begain = 86 + 4 * 44.f + 10.f;
-    //    for (int i = 0; i < 4; i++) {
-    //        UIImageView *image = [self imageViewWithColorArray:@[UIColorHex(0x1E90FF), UIColorHex(0xBA55D3), UIColorHex(0xF47983), UIColorHex(0xF47983), UIColorHex(0xFFD700), UIColorHex(0x40E0D0)] size:CGSizeMake(kScreenWidth - 20.f, 44) direction:i];
-    //        image.centerX = self.view.centerX;
-    //        image.top = begain + i * 44.f;
-    //        [self.view addSubview:image];
-    //    }
-    
-    CGFloat begain2 = begain + 0 * 44.f + 10.f;
-    for (int i = 0; i < 4; i++) {
-        UIImageView *image = [self imageViewWithFromColor:UIColorHex(0x1E90FF) toColor:UIColorHex(0xBA55D3) size:CGSizeMake(kScreenWidth - 20.f, 44) direction:i];
-        image.centerX = self.view.centerX;
-        image.top = begain2 + i * 44.f;
-        [self.view addSubview:image];
+    if ([cell respondsToSelector:@selector(setLayoutMargins:)])
+    {
+        [cell setLayoutMargins:UIEdgeInsetsMake(0, 0, 0, 0)];
     }
-    
-    UIImage *image = [UIImage lt_imageWithFromColor:UIColorHex(0x1E90FF) toColor:UIColorHex(0xBA55D3) size:CGSizeMake(kScreenWidth - 20.f, 200.f) direction:LTGradientImageDirectionLeft];
-    UIImageView *imageView = [[UIImageView alloc] initWithImage:[image lt_imageWithGradientAlphaDirection:LTGradientImageDirectionLeft]];
-    imageView.centerX = self.view.centerX;
-    imageView.top = begain2 + 4 * 44.f + 10.f;
-    [self.view addSubview:imageView];
 }
 
-- (UIImageView *)imageViewWithFromColor:(UIColor *)fromColor toColor:(UIColor *)toColor size:(CGSize)size direction:(LTGradientImageDirection)direction
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    UIImage *image = [UIImage lt_imageWithFromColor:fromColor toColor:toColor size:size direction:direction];
-    UIImageView *imageView = [[UIImageView alloc] initWithImage:image];
-    [imageView sizeToFit];
-    return imageView;
+    static NSString * const identifier = @"identifier";
+    UITableViewCell * cell = [tableView dequeueReusableCellWithIdentifier:identifier];
+    if (!cell)
+    {
+        cell = [[UITableViewCell alloc]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:identifier];
+    }
+    cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+    cell.textLabel.text = _titleArray[indexPath.row];
+    cell.textLabel.textAlignment = 0;
+    cell.textLabel.font = [UIFont boldSystemFontOfSize:12];
+    [cell.textLabel setTextColor:[UIColor darkGrayColor]];
+    cell.selectionStyle = UITableViewCellSelectionStyleDefault;
+    return cell;
 }
 
-- (UIImageView *)imageViewWithColorArray:(NSArray *)colorArray size:(CGSize)size direction:(LTGradientImageDirection)direction
-{
-    UIImage *image = [UIImage lt_imageWithColorArray:colorArray size:size direction:direction];
-    UIImageView *imageView = [[UIImageView alloc] initWithImage:image];
-    [imageView sizeToFit];
-    return imageView;
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    switch (indexPath.row)
+    {
+        case 0:
+        {
+            [LTPrivacyPermission.sharedPermission accessPrivacyPermissionWithType:LTPrivacyPermissionTypePhoto completion:^(BOOL authorized, LTPrivacyPermissionAuthorizationStatus status) {
+                
+            }];
+        }
+            break;
+            
+        case 1:
+        {
+            [LTPrivacyPermission.sharedPermission accessPrivacyPermissionWithType:LTPrivacyPermissionTypeCamera completion:^(BOOL authorized, LTPrivacyPermissionAuthorizationStatus status) {
+                
+            }];
+        }
+            break;
+            
+        case 2:
+        {
+            [LTPrivacyPermission.sharedPermission accessPrivacyPermissionWithType:LTPrivacyPermissionTypeMedia completion:^(BOOL authorized, LTPrivacyPermissionAuthorizationStatus status) {
+                
+            }];
+        }
+            break;
+            
+        case 3:
+        {
+            [LTPrivacyPermission.sharedPermission accessPrivacyPermissionWithType:LTPrivacyPermissionTypeMicrophone completion:^(BOOL authorized, LTPrivacyPermissionAuthorizationStatus status) {
+                
+            }];
+        }
+            break;
+            
+        case 4:
+        {
+            [LTPrivacyPermission.sharedPermission accessPrivacyPermissionWithType:LTPrivacyPermissionTypeLocationAlwaysAndWhenInUse completion:^(BOOL authorized, LTPrivacyPermissionAuthorizationStatus status) {
+                
+            }];
+        }
+            break;
+            
+        case 5:
+        {
+            [LTPrivacyPermission.sharedPermission accessPrivacyPermissionWithType:LTPrivacyPermissionTypePushNotification completion:^(BOOL authorized, LTPrivacyPermissionAuthorizationStatus status) {
+                
+            }];
+        }
+            break;
+            
+        case 6:
+        {
+            [LTPrivacyPermission.sharedPermission accessPrivacyPermissionWithType:LTPrivacyPermissionTypeSpeech completion:^(BOOL authorized, LTPrivacyPermissionAuthorizationStatus status) {
+                
+            }];
+        }
+            break;
+            
+        case 7:
+        {
+            [LTPrivacyPermission.sharedPermission accessPrivacyPermissionWithType:LTPrivacyPermissionTypeEvent completion:^(BOOL authorized, LTPrivacyPermissionAuthorizationStatus status) {
+                
+            }];
+        }
+            break;
+            
+        case 8:
+        {
+            [LTPrivacyPermission.sharedPermission accessPrivacyPermissionWithType:LTPrivacyPermissionTypeContact completion:^(BOOL authorized, LTPrivacyPermissionAuthorizationStatus status) {
+                
+            }];
+        }
+            break;
+            
+        case 9:
+        {
+            [LTPrivacyPermission.sharedPermission accessPrivacyPermissionWithType:LTPrivacyPermissionTypeReminder  completion:^(BOOL authorized, LTPrivacyPermissionAuthorizationStatus status) {
+                
+            }];
+        }
+            break;
+            
+        default:
+            break;
+    }
+    [tableView reloadData];
 }
 
-- (UIImageView *)imageViewWithColor:(UIColor *)color size:(CGSize)size direction:(LTGradientImageDirection)direction
-{
-    UIImage *image = [UIImage lt_imageWithColor:color size:size direction:direction];
-    UIImageView *imageView = [[UIImageView alloc] initWithImage:image];
-    [imageView sizeToFit];
-    return imageView;
-}
 
 @end
