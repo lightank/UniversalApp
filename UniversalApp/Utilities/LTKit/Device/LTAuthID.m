a81f8bbb94bde94f98dfb9693aec96ff811b6bc9
huanyu.li <lihuanyux@163.com>
2019-04-27 22:55:18 +0800

    no message

--- UniversalApp/Utilities/LTKit/Device/LTAuthID.m
+++ UniversalApp/Utilities/LTKit/Device/LTAuthID.m
@@ -13,8 +13,8 @@
 
 + (void)showAuthIDWithReason:(NSString *)reason fallbackTitle:(NSString *)fallbackTitle block:(LTAuthIDStateBlock)block
 {
-    NSString *localizedReason = reason ? : @"";
-    NSString *localizedFallbackTitle = fallbackTitle ? : @"";
+    NSString *localizedReason = @"请验证信息";
+    NSString *localizedFallbackTitle = @"输入密码";
     
     
     if ([[[UIDevice currentDevice] systemVersion] doubleValue] < 8.0)
@@ -27,10 +27,10 @@
     }
     
     LAContext *context = [[LAContext alloc] init];
-    // iOS 10 以后可以设置取消的title
+    // iOS 10 以后可以设置取消的title,不能为空字符串
     //context.localizedCancelTitle = @"取消";
-    // 认证失败提示信息，为 @"" 则不提示
-    context.localizedFallbackTitle = localizedFallbackTitle;
+    // 认证失败提示信息，不能为空字符串
+    //context.localizedFallbackTitle = localizedFallbackTitle;
     
     NSError *error = nil;
     // LAPolicyDeviceOwnerAuthenticationWithBiometrics: 用TouchID/FaceID验证
@@ -43,148 +43,76 @@
             {
                 state = LTAuthIDStateSuccess;
             }
-            else if(error)
+            
+            if (error)
             {
-                if (@available(iOS 11.0, *))
+                switch (error.code)
                 {
-                    switch (error.code)
+                    case LAErrorAuthenticationFailed:
                     {
-                        case LAErrorAuthenticationFailed:
-                        {
-                            NSLog(@"TouchID/FaceID 验证失败");
-                            state = LTAuthIDStateFail;
-                        }
-                            break;
-                        case LAErrorUserCancel:
-                        {
-                            NSLog(@"TouchID/FaceID 被用户手动取消");
-                            state = LTAuthIDStateUserCancel;
-                        }
-                            break;
-                        case LAErrorUserFallback:
-                        {
-                            NSLog(@"用户不使用TouchID/FaceID,选择手动输入密码");
-                            state = LTAuthIDStateInputPassword;
-                        }
-                            break;
-                        case LAErrorSystemCancel:
-                        {
-                            NSLog(@"TouchID/FaceID 被系统取消 (如遇到来电,锁屏,按了Home键等)");
-                            state = LTAuthIDStateSystemCancel;
-                        }
-                            break;
-                        case LAErrorPasscodeNotSet:
-                        {
-                            NSLog(@"TouchID/FaceID 无法启动,因为用户没有设置密码");
-                            state = LTAuthIDStatePasswordNotSet;
-                        }
-                            break;
-                            //case LAErrorTouchIDNotEnrolled:{
-                        case LAErrorBiometryNotEnrolled:
-                        {
-                            NSLog(@"TouchID/FaceID 无法启动,因为用户没有设置TouchID/FaceID");
-                            state = LTAuthIDStateTouchIDNotSet;
-                        }
-                            break;
-                            //case LAErrorTouchIDNotAvailable:{
-                        case LAErrorBiometryNotAvailable:
-                        {
-                            NSLog(@"TouchID/FaceID 无效");
-                            state = LTAuthIDStateTouchIDNotAvailable;
-                        }
-                            break;
-                            //case LAErrorTouchIDLockout:{
-                        case LAErrorBiometryLockout:
-                        {
-                            NSLog(@"TouchID/FaceID 被锁定(连续多次验证TouchID/FaceID失败,系统需要用户手动输入密码)");
-                            state = LTAuthIDStateTouchIDLockout;
-                        }
-                            break;
-                        case LAErrorAppCancel:
-                        {
-                            NSLog(@"当前软件被挂起并取消了授权 (如App进入了后台等)");
-                            state = LTAuthIDStateAppCancel;
-                        }
-                            break;
-                        case LAErrorInvalidContext:
-                        {
-                            NSLog(@"当前软件被挂起并取消了授权 (LAContext对象无效)");
-                            state = LTAuthIDStateInvalidContext;
-                        }
-                            break;
-                        default:
-                            break;
+                        NSLog(@"TouchID/FaceID 验证失败");
+                        state = LTAuthIDStateFail;
                     }
-                }
-                else
-                {
-                    // iOS 11.0以下的版本只有 TouchID 认证
-                    switch (error.code)
+                        break;
+                    case LAErrorUserCancel:
                     {
-                        case LAErrorAuthenticationFailed:
-                        {
-                            NSLog(@"TouchID 验证失败");
-                            state = LTAuthIDStateFail;
-                        }
-                            break;
-                        case LAErrorUserCancel:
-                        {
-                            NSLog(@"TouchID 被用户手动取消");
-                            state = LTAuthIDStateUserCancel;
-                        }
-                            break;
-                        case LAErrorUserFallback:
-                        {
-                            NSLog(@"用户不使用TouchID,选择手动输入密码");
-                            state = LTAuthIDStateInputPassword;
-                        }
-                            break;
-                        case LAErrorSystemCancel:
-                        {
-                            NSLog(@"TouchID 被系统取消 (如遇到来电,锁屏,按了Home键等)");
-                            state = LTAuthIDStateSystemCancel;
-                        }
-                            break;
-                        case LAErrorPasscodeNotSet:
-                        {
-                            NSLog(@"TouchID 无法启动,因为用户没有设置密码");
-                            state = LTAuthIDStatePasswordNotSet;
-                        }
-                            break;
-                        case LAErrorTouchIDNotEnrolled:
-                        {
-                            NSLog(@"TouchID 无法启动,因为用户没有设置TouchID");
-                            state = LTAuthIDStateTouchIDNotSet;
-                        }
-                            break;
-                            //case :{
-                        case LAErrorTouchIDNotAvailable:
-                        {
-                            NSLog(@"TouchID 无效");
-                            state = LTAuthIDStateTouchIDNotAvailable;
-                        }
-                            break;
-                        case LAErrorTouchIDLockout:
-                        {
-                            NSLog(@"TouchID 被锁定(连续多次验证TouchID失败,系统需要用户手动输入密码)");
-                            state = LTAuthIDStateTouchIDLockout;
-                        }
-                            break;
-                        case LAErrorAppCancel:
-                        {
-                            NSLog(@"当前软件被挂起并取消了授权 (如App进入了后台等)");
-                            state = LTAuthIDStateAppCancel;
-                        }
-                            break;
-                        case LAErrorInvalidContext:
-                        {
-                            NSLog(@"当前软件被挂起并取消了授权 (LAContext对象无效)");
-                            state = LTAuthIDStateInvalidContext;
-                        }
-                            break;
-                        default:
-                            break;
+                        NSLog(@"TouchID/FaceID 被用户手动取消");
+                        state = LTAuthIDStateUserCancel;
                     }
+                        break;
+                    case LAErrorUserFallback:
+                    {
+                        NSLog(@"用户不使用TouchID/FaceID,选择手动输入密码");
+                        state = LTAuthIDStateInputPassword;
+                    }
+                        break;
+                    case LAErrorSystemCancel:
+                    {
+                        NSLog(@"TouchID/FaceID 被系统取消 (如遇到来电,锁屏,按了Home键等)");
+                        state = LTAuthIDStateSystemCancel;
+                    }
+                        break;
+                    case LAErrorPasscodeNotSet:
+                    {
+                        NSLog(@"TouchID/FaceID 无法启动,因为用户没有设置密码");
+                        state = LTAuthIDStatePasswordNotSet;
+                    }
+                        break;
+                    case LAErrorTouchIDNotEnrolled:
+                    //case LAErrorBiometryNotEnrolled:
+                    {
+                        NSLog(@"TouchID/FaceID 无法启动,因为用户没有设置TouchID/FaceID");
+                        state = LTAuthIDStateTouchIDNotSet;
+                    }
+                        break;
+                    case LAErrorTouchIDNotAvailable:
+                    //case LAErrorBiometryNotAvailable:
+                    {
+                        NSLog(@"TouchID/FaceID 无效");
+                        state = LTAuthIDStateTouchIDNotAvailable;
+                    }
+                        break;
+                    case LAErrorTouchIDLockout:
+                    //case LAErrorBiometryLockout:
+                    {
+                        NSLog(@"TouchID/FaceID 被锁定(连续多次验证TouchID/FaceID失败,系统需要用户手动输入密码)");
+                        state = LTAuthIDStateTouchIDLockout;
+                    }
+                        break;
+                    case LAErrorAppCancel:
+                    {
+                        NSLog(@"当前软件被挂起并取消了授权 (如App进入了后台等)");
+                        state = LTAuthIDStateAppCancel;
+                    }
+                        break;
+                    case LAErrorInvalidContext:
+                    {
+                        NSLog(@"当前软件被挂起并取消了授权 (LAContext对象无效)");
+                        state = LTAuthIDStateInvalidContext;
+                    }
+                        break;
+                    default:
+                        break;
                 }
             }
             
