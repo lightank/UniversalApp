2ae99a5c33585ba4356cee1992e155f40b695968
huanyu.li <lihuanyux@163.com>
2020-08-12 22:14:45 +0800

    no message

---
+++ UniversalApp/Utilities/LTKit/LTCore/Foundation/NSDate+LTAdd.h
@@ -0,0 +1,17 @@
+//
+//  NSDate+LTAdd.h
+//  UniversalApp
+//
+//  Created by huanyu.li on 2020/8/12.
+//  Copyright © 2020 huanyu.li. All rights reserved.
+//  see Formatting Dates and Times at http://userguide.icu-project.org/formatparse/datetime
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NSDate (LTAdd)
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ UniversalApp/Utilities/LTKit/LTCore/Foundation/NSDate+LTAdd.m
@@ -0,0 +1,16 @@
+//
+//  NSDate+LTAdd.m
+//  UniversalApp
+//
+//  Created by huanyu.li on 2020/8/12.
+//  Copyright © 2020 huanyu.li. All rights reserved.
+//
+
+#import "NSDate+LTAdd.h"
+
+#import <AppKit/AppKit.h>
+
+
+@implementation NSDate (LTAdd)
+
+@end
